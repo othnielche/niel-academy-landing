792395e4168389ceb8e170c51980f8639cec0bbf
Othniel Che <cheothniel@gmail.com>
2025-10-14 11:44:09 +0100

    sorted,,,

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,5 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
-import { Badge } from '../components/ui/badge';
-import { Separator } from '../components/ui/separator';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
+
 
 export function About() {
   return (
